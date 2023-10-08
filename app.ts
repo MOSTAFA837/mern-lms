cb943655be8a33a5147b53ffe9189a613e22bcef
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-10-08 14:12:16 +0200

    server initializing

---
+++ app.ts
@@ -0,0 +1,26 @@
+require("dotenv").config();
+import cookieParser from "cookie-parser";
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+
+export const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({ success: true, message: "API is working." });
+});
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
